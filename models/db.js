c418ddeb9cc5147adbc1cb0cb5e155b903fe7b61
Alex Filatov <tanyaarni@gmail.com>
2020-06-10 15:09:26 +0300

    heroku

--- models/db.js
+++ models/db.js
@@ -1,30 +1,30 @@
-// const { Pool } = require('pg');
-//
-// const pool = new Pool({
-//     user: process.env.DB_USSER,
-//     host: process.env.DB_HOST,
-//     database: 'url',
-// });
-//
-// pool.connect();
-
-// pool.query(`
-//     do $$
-//     begin
-//         call transfer();
-//     end
-//     $$;`);
-
-// module.exports = pool;
-
 const { Pool } = require('pg');
+
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
+    user: process.env.DB_USSER,
+    host: process.env.DB_HOST,
+    database: 'url',
 });
 
 pool.connect();
 
+pool.query(`
+    do $$
+    begin
+        call transfer();
+    end
+    $$;`);
+
 module.exports = pool;
+
+// const { Pool } = require('pg');
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
+// });
+//
+// pool.connect();
+//
+// module.exports = pool;
